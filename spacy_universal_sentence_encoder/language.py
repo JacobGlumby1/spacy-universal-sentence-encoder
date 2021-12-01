a0c70b295ac8176fbd5c2fb9033806a00c0772a2
JacobGlumby1 <43093373+JacobGlumby1@users.noreply.github.com>
2021-12-01 13:56:43 +0100

    Update language.py

--- spacy_universal_sentence_encoder/language.py
+++ spacy_universal_sentence_encoder/language.py
@@ -4,6 +4,8 @@ from spacy.language import Language
 from spacy.tokens import Doc, Span, Token
 from spacy.util import get_lang_class
 from absl import logging
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"  
 import tensorflow as tf
 import tensorflow_hub as hub
 try:
